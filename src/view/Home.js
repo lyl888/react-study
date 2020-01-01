3aba1abd4281f88e09bef9f0f878d5bdec12d1ed
610 <740898255@qq.com>
2020-01-01 16:27:45 +0800

    更改git 提交限制

--- src/view/Home.js
+++ src/view/Home.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import Headers from '../components/headers/Index';
-import Login from '../components/logins/Index';
 function Home() {
   return (
     <div>
       <Headers />
-      <Login />
     </div>
   );
 }
